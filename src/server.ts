8691b2823a6cc9a1ac100e80d923f74825ae10f4
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2024-04-12 19:37:35 -0300

    config prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,17 @@ import fastify from "fastify";
 
 const app = fastify()
 
+// Rotas
+app.get('/', () => {
+    return 'Hello meu guri!'
+})
+
+// Rotas
+app.get('/test', () => {
+    return 'Teste meu guri!'
+})
+
+
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
