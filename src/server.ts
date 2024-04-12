da5030715c298b739273bfe15fa33ae2f2ee8213
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2024-04-12 20:22:49 -0300

    connect DB and create base backend

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,34 @@
 import fastify from "fastify";
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
 
-// Rotas
-app.get('/', () => {
-    return 'Hello meu guri!'
+const prisma = new PrismaClient({
+    log: ['query']
 })
 
 // Rotas
-app.get('/test', () => {
-    return 'Teste meu guri!'
+// Request resquisições / reply 
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximiumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        },
+    })
+
+    return reply.status(201).send({ eventid: event.id })
 })
 
 
